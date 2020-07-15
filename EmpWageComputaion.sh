5c6bdcc283a722f67353b88a41a8d3b5332f7e58
Ravi9038 <ravijadhav9038@gmail.com>
2020-07-15 08:31:15 +0530

    Checking the present absent

--- EmpWageComputaion.sh
+++ EmpWageComputaion.sh
@@ -2,3 +2,15 @@
 
 echo "Welcome to Employee Wage Computation."
 
+#Use Case 2
+
+empCheck=$(( RANDOM%2 ))
+
+if [[ $empCheck -eq 1 ]]
+then
+	echo "Employee is Present"
+else
+	echo "Employee is Absent"
+fi
+
+
