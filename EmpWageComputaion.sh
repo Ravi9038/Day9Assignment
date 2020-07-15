91b3f6097f635b9e22433906c3d44ea21f47233e
Ravi9038 <ravijadhav9038@gmail.com>
2020-07-15 08:41:12 +0530

    Calculate the Daily Wage

--- EmpWageComputaion.sh
+++ EmpWageComputaion.sh
@@ -2,15 +2,13 @@
 
 echo "Welcome to Employee Wage Computation."
 
-#Use Case 2
+#Use Case 1
 
 empCheck=$(( RANDOM%2 ))
 
-if [[ $empCheck -eq 1 ]]
-then
-	echo "Employee is Present"
-else
-	echo "Employee is Absent"
-fi
+#Use Case 2
+empHrs=8;
+empHrsPerRate=20;
 
 
+ 
